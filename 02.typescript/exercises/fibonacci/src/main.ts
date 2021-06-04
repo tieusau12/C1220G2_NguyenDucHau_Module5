68b99edaf3eab0324d19b6ef0fc56e746285f43c
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-06-04 13:37:37 +0700

    re-up fibonacci

--- 02.typescript/exercises/fibonacci/src/main.ts
+++ 02.typescript/exercises/fibonacci/src/main.ts
@@ -1,4 +1,4 @@
-function fibonacci(number: number): any {
+function fibonacci(number: number): number {
     if (number < 1) {
         return 0;
     } else if (number <= 2) {
@@ -6,10 +6,11 @@ function fibonacci(number: number): any {
     }
     return fibonacci(number - 1) + fibonacci(number - 2);
 }
-let sum=0;
-let n=20;
-for (let i=0;i<=n;i++){
-    sum+=fibonacci(i);
+
+let sum: number = 0;
+let n: number = 20;
+for (let i = 0; i <= n; i++) {
+    sum += fibonacci(i);
 }
 console.log(sum);
 
